86a966a7f1abcf4fca9be5c72924184ac2b64de9
lesegot-code <lesegot2005@gmail.com>
2026-09-03 23:46:25 +0200

    feat/lesego: Rename Backend class to CompositeWorkItem

--- CompositeWorkItem.h
+++ CompositeWorkItem.h
@@ -1,5 +1,5 @@
-#ifndef BACKEND_H
-#define BACKEND_H
+#ifndef COMPOSITEWORKITEM_H
+#define COMPOSITEWORKITEM_H
 
 #include "WorkItem.h"
 
@@ -11,7 +11,7 @@
  *
  * A Backend can contain individual work items and nested groups.
 */
-class Backend : public WorkItem{
+class CompositeWorkItem : public WorkItem{
     private:
         /** 
          * @brief Work items contained directly in this backend group. 
@@ -23,7 +23,7 @@ class Backend : public WorkItem{
          * @brief Creates a backend group.
          * @param name The name of the backend group.
         */
-        explicit Backend(const std::string& name);
+        explicit CompositeWorkItem(const std::string& name);
 
         /**
          * @brief Executes all work items contained in the backend group.
@@ -86,7 +86,7 @@ class Backend : public WorkItem{
         /**
          * @brief Destroys the backend group and its owned children.
         */
-        ~Backend() override;
+        ~CompositeWorkItem() override;
 };
 
-#endif // BACKEND_H
+#endif // COMPOSITEWORKITEM_H
