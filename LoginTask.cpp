3e8ffb5ff01555a9281fb852c887e99e95d8b810
lesegot <lesegot2005@gmail.com>
2026-09-03 20:31:06 +0200

    feat/lesego: Implemented LoginTask.cpp, Incomplete for now, as I needs the State DP

---
+++ LoginTask.cpp
@@ -0,0 +1,42 @@
+#include "LoginTask.h"
+
+#include <iostream>
+
+LoginTask::LoginTask(const std::string& name, const std::string& authenticationMethod, bool requiresTwoFactor)
+    : WorkItem(name), authenticationMethod(authenticationMethod), requiresTwoFactor(requiresTwoFactor)
+{}
+
+void LoginTask::execute(){
+    std::cout << "Executing login task: " << getName() << std::endl;
+    std::cout << "Authentication method: " << authenticationMethod << std::endl;
+
+    if(requiresTwoFactor){
+        std::cout << "2FA configuration required." << std::endl;
+    }
+}
+
+std::string LoginTask::getDescription() const{
+    std::string description = "Login task: " + getName();
+
+    if(requiresTwoFactor){
+        description += " (2FA required)";
+    }
+
+    return description;
+}
+
+std::string LoginTask::getStatus() const{
+    return state->getName(); //needs the State DP
+}
+
+bool LoginTask::start(){
+    return state->start(*this); //needs the State DP
+}
+
+bool LoginTask::block(){
+    return state->block(*this); //needs the State DP
+}
+
+bool LoginTask::complete(){
+    return state->complete(*this); //needs the State DP
+}
