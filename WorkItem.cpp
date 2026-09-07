b64fe66a7b1157417e282112fe5b31ee5a33607a
lesegot-code <lesegot2005@gmail.com>
2026-09-07 10:49:33 +0200

    feat/lesego: Implement virtual detach method in WorkItem
    Add virtual detach method to WorkItem class.

--- WorkItem.cpp
+++ WorkItem.cpp
@@ -51,6 +51,12 @@ bool WorkItem::remove(WorkItem* item){
     return false;
 }
 
+virtual WorkItem* WorkItem::detach(WorkItem* item){
+    //empty for leaves only composites will override it
+    (void)item;
+    return nullptr;
+}
+
 int WorkItem::getChildCount() const{
     //empty for leaves only composites will override it
     return 0;
